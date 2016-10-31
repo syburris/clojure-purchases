e78e2e045e450534ba5584e8eeda58e5d5d9e381
Steven Burris <stevenburris.jr@gmail.com>
2016-10-31 13:22:36 -0400

    Filtered purchases.csv

--- src/clojure_purchases/core.clj
+++ src/clojure_purchases/core.clj
@@ -1,6 +1,24 @@
 (ns clojure-purchases.core
+  (:require [clojure.string :as str])
   (:gen-class))
 
+(def file-name "purchases.csv")
+
 (defn -main [& args]
-  (println "Hello, world!"))
+  (print "Type a purchase category: ")
+  (let [purchases (str/split-lines(slurp file-name))
+        purchases (map (fn [line]
+                         (str/split line #","))
+                    purchases)
+        header (first purchases)
+        purchases (rest purchases)
+        purchases (map (fn [line]
+                         (zipmap header line))
+                    purchases)
+        purchase-category (read-line)
+        purchases (filter (fn [line]
+                            (= (get line "category") purchase-category))
+                    purchases)
+        file-text (pr-str purchases)]
+    (spit "filtered_purchases.edn" file-text)))
 
